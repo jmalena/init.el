39b8aa5fc55bc5c4599b68be2ff0139567e6ba4f
Jonáš Malena <jonas.malena@gmail.com>
2024-11-17 00:52:57 +0100

    add helm

---
+++ init.el
@@ -0,0 +1,21 @@
+(eval-when-compile
+  (require 'use-package))
+
+(use-package helm
+  :bind (("M-x" . helm-M-x)
+         ("C-x b" . helm-mini)
+         ("C-x C-f" . helm-find-files))
+  :config ((helm-mode 1)))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custocm.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(helm)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
