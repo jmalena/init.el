bdb96b14ef3b40b6038b2e3ce9a595087f7d2882
Jonáš Malena <jonas.malena@gmail.com>
2024-11-17 01:25:10 +0100

    setup package.el repositories

--- init.el
+++ init.el
@@ -1,5 +1,15 @@
-(eval-when-compile
-  (require 'use-package))
+;; Setup package.el repositories
+(require 'package)
+(add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; Setup use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
 
 (use-package emacs
   :init
@@ -13,10 +23,10 @@
   :bind (("M-x" . helm-M-x)
          ("C-x b" . helm-mini)
          ("C-x C-f" . helm-find-files))
-  :config ((helm-mode 1)))
+  :config (helm-mode 1))
 
 (custom-set-variables
- ;; custom-set-variables was added by Custocm.
+ ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
